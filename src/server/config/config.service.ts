4559530c0c8c240c1370949356d0b7ac1c9d8c49
Joel Campos <joelcamposoliva9000@gmail.com>
2023-05-21 16:20:11 +0200

    Add database env config

--- src/server/config/config.service.ts
+++ src/server/config/config.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Env } from '../../types/env/env.type';
 
+const DEFAULT_REPOSITORY_URL = 'https://github.com/TheMineWay/otaku-bot';
+const DEFAULT_DATABASE_PORT = 3306;
+
 @Injectable()
 export class ConfigService {
   private readonly env: EnvModel;
@@ -9,8 +12,10 @@ export class ConfigService {
     const env = process.env as Env;
     this.env = {
       ...env,
-      REPOSITORY_URL:
-        env.REPOSITORY_URL ?? 'https://github.com/TheMineWay/otaku-bot',
+      REPOSITORY_URL: env.REPOSITORY_URL ?? DEFAULT_REPOSITORY_URL,
+      DATABASE_PORT: env.DATABASE_PORT
+        ? +env.DATABASE_PORT
+        : DEFAULT_DATABASE_PORT,
     };
   }
 
@@ -19,5 +24,12 @@ export class ConfigService {
 
 class EnvModel {
   DISCORD_TOKEN: string;
+
+  DATABASE_NAME: string;
+  DATABASE_USER: string;
+  DATABASE_PASSWORD: string;
+  DATABASE_HOST: string;
+  DATABASE_PORT: number;
+
   REPOSITORY_URL: string;
 }
--- src/types/env/env.type.ts
+++ src/types/env/env.type.ts
@@ -1,4 +1,11 @@
 export type Env = {
   DISCORD_TOKEN: string;
+
+  DATABASE_NAME: string;
+  DATABASE_USER: string;
+  DATABASE_PASSWORD: string;
+  DATABASE_HOST: string;
+  DATABASE_PORT?: string;
+
   REPOSITORY_URL?: string;
 };
